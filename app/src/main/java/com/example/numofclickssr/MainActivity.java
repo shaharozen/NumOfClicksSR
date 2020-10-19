0bd2cc1d19e58c8959a84545603a45c32b58f581
Shahar Rozenberg <rz.shahar@gmail.com>
2020-10-19 19:07:26 +0300

    Done

--- app/src/main/java/com/example/numofclickssr/MainActivity.java
+++ app/src/main/java/com/example/numofclickssr/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.numofclickssr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int cnt=0;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.txtScore);
+    }
+
+    public void methodCounter(View view) {
+        cnt++;
+        String str=getString(R.string.textOnTextBox);
+        tv.setText(str+" "+cnt);
     }
 }
